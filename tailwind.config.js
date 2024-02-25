dd49e33d8aee9a5fa7b49985bde6d6bd215552cb
Sarah Montenegro <sarah-yasmin13@hotmail.com>
2024-02-25 12:08:30 -0300

    projeto remodelado

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        purple : '#7723C9',       
+      },
+
+      fontFamily: {
+        nunito : ['Nunito', 'sans-serif']
+      },
+
+      spacing : {
+        7.5 : '30px',
+        15 : '59.5px',
+        17 : '68px',
+        18 : '72px',
+        22 : '88px',
+      }
+    },
+  },
+  plugins: [],
+}
+
